b4cda3b59a586e567bdeb8af02087e35a6ca4de5
Michael van der Weg <michael@vanderweg.ch>
2013-07-13 20:56:32 +0200

    fixed some bugs in the middleware connector

--- test.js
+++ test.js
@@ -1,14 +1,46 @@
 
 
-	var  Webfiles 	= require( "./" )
-	   , log 		= require( "ee-log" );
+	var   Webfiles 		= require( "./" )
+		, Webservice 	= require( "../ee-webservice" )
+	   	, log 			= require( "ee-log" );
 
 
 	var files = new Webfiles();
 
 
-	files.load( __dirname + "/test/files", function( err ){
-		if ( err ) throw err;
-		log.dir( files.hashTree );
-		log.dir( files.tree );
+	// serve index.html, index.json & default.htm files as directory index
+	files.addDirectoryIndex( [ "index.html", "index.json" ] );
+	files.addDirectoryIndex( "default.htm" );
+
+
+	// don't serve dot, mustache & .php files
+	files.addFilter( [ /\.mustache$/, /^\/\./ ] );
+	files.addFilter( /\.php$/ );
+	
+
+	var service = new Webservice( {
+		webserver: {
+			http: {
+				  interface: 	Webservice.Webserver.IF_ANY 
+				, port: 		12001
+			}
+		}
 	} );
+
+	service.use( files );
+
+
+	files.load( __dirname + "/test/files", function( err ){
+		service.listen();
+	} );
+
+
+
+
+	
+
+
+	
+
+
+	
