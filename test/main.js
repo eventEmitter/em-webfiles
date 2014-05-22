bf3334ce78833ec0fe967bf7cecf4c803611f551
Michael van der Weg <michael@joinbox.com>
2014-05-22 19:37:26 +0200

    better logs

--- test/main.js
+++ test/main.js
@@ -61,7 +61,7 @@
 
 		it('should return the correct file on a request', function(done){
 			files.request(new Request('/dir1/dir1_1/test.txt'), new Response(function(file, headers, status){
-				assert.equal(JSON.stringify(file), '{"type":"Buffer","data":[116,101,115,116,32]}');
+				assert.equal(file.toString().length, 5);
 				assert.equal(JSON.stringify(headers), '{"Etag":"a2b2d66938b1f023dec6394f12b782b5","Content-Type":"text/plain; charset=UTF-8"}');
 				assert.equal(status, 200);
 				done();
@@ -75,7 +75,7 @@
 			files.addDirectoryIndex('index.html');
 
 			files.request(new Request('/dir1/'), new Response(function(file, headers, status){
-				assert.equal(JSON.stringify(file), '{"type":"Buffer","data":[60,104,116,109,108,62,10,60,104,101,97,100,62,10,9,60,116,105,116,108,101,62,104,105,104,111,60,47,116,105,116,108,101,62,10,60,47,104,101,97,100,62,10,60,98,111,100,121,62,10,105,116,115,32,109,101,32,58,41,10,60,47,98,111,100,121,62,10,60,47,104,116,109,108,62]}');
+				assert.equal(file.toString().length, 75);
 				assert.equal(JSON.stringify(headers), '{"Etag":"4f5f6a5e6fcdee16bf222f1d18c120ec","Content-Type":"text/html; charset=UTF-8"}');
 				assert.equal(status, 200);
 				done();
